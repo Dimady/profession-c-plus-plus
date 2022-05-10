317d4d13c9e58fe8827c92c62efab5d7dea00ddb
Dmitriy <romashka14108@mail.ru>
2022-05-10 23:58:37 +0300

    homework31

---
+++ homework31.2/matrixgraph.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <string>
+#include "igraph.h"
+#include "listgraph.h"
+
+class MatrixGraph : public IGraph {
+private:
+    std::string type = "Matrix";
+    std::vector<std::vector<int>> grph;
+
+public:
+    MatrixGraph();
+
+    MatrixGraph(IGraph *_oth);
+
+    virtual ~MatrixGraph();
+
+    std::string getType();
+
+    void AddEdge(int from, int to); // Метод принимает вершины начала и конца ребра и добавляет ребро
+
+    int VerticesCount() const; // Метод должен считать текущее количество вершин
+
+    void GetNextVertices(int vertex, std::vector<int> &vertices) const; // Для конкретной вершины метод выводит в вектор “вершины”
+                                                                                    // все вершины, в которые можно дойти по ребру из данной
+
+    void GetPrevVertices(int vertex, std::vector<int> &vertices) const; // Для конкретной вершины метод выводит в вектор “вершины”
+                                                                                    // все вершины, из которых можно дойти по ребру в данную
+
+};
